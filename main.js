617a2be2467bcf504ed4696f8e320572c5f6f6d9
Erlan-asd <Vipoffacc@gmail.com>
2024-03-18 18:35:55 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// 1)---------------------------------------------------------
+// function removeSpaces(str) {
+//     return str.replace(/\s/g, '');
+//     }
+    
+//     let stringWithoutSpaces = removeSpaces("    hello my friends   ");
+//     console.log(stringWithoutSpaces); 
+
+
+// 2)---------------------------------------------------------
+
+
+// function last(arr) {
+//     return arr[arr.length - 1];
+//     }
+//     const array = [1, 2, 3, 4, 5];
+//     console.log(last(array)); 
+
+// 3)---------------------------------------------------------
+
+
+// function con(arr1, arr2) {
+//     return arr1.concat(arr2);
+//     }
+    
+//     const array1 = [1, 2, 3];
+//     const array2 = [4, 5, 6];
+//     console.log(con(array1, array2)); 
+
+// 4)---------------------------------------------------------
+
+// function capitalizeFirstLetter(str) {
+//     return str.charAt(0).toUpperCase() + str.slice(1);
+//     }
+
+// console.log(capitalizeFirstLetter("hello")); 
